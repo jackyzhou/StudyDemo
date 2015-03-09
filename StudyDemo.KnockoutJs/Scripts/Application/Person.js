8e66d90ee39d464d6a7bb3e61f7c6a7bb54c67ed
jackyzhou <80103558@qq.com>
2015-03-09 10:43:56 +0800

    xx

--- StudyDemo.KnockoutJs/Scripts/Application/Person.js
+++ StudyDemo.KnockoutJs/Scripts/Application/Person.js
@@ -35,31 +35,27 @@
 
         $('#Save').click(function (e) {
 
-            // Check whether the form is valid. Note: Remove this check, if you are not using HTML5
-            if (document.forms[0].checkValidity()) {
+            e.preventDefault();  //取消事件的默认动作。
 
-                e.preventDefault();
-
-                $.ajax({
-                    type: "POST",
-                    url: Person.SaveUrl,
-                    data: ko.toJSON(Person.ViewModel),
-                    contentType: 'application/json',
-                    async: true,
-                    beforeSend: function () {
-                        // Display loading image
-                    },
-                    success: function (result) {
-                        // Handle the response here.
-                    },
-                    complete: function () {
-                        // Hide loading image.
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) {
-                        // Handle error.
-                    }
-                });
-            }
+            $.ajax({
+                type: "POST",
+                url: Person.SaveUrl,
+                data: ko.toJSON(Person.ViewModel),
+                contentType: 'application/json',
+                async: true,
+                beforeSend: function () {
+                    // Display loading image
+                },
+                success: function (result) {
+                    // Handle the response here.
+                },
+                complete: function () {
+                    // Hide loading image.
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    // Handle error.
+                }
+            });
         });
     },
 };
